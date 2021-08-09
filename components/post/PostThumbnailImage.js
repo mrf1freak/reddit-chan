8ca60708fade3848e6e09c76a0afcc35851e59d2
Saad Ahmad <saadahmad96@hotmail.com>
2021-08-09 12:10:10 +0500

    Added links to images

--- components/post/PostThumbnailImage.js
+++ components/post/PostThumbnailImage.js
@@ -1,19 +1,22 @@
 import Image from 'next/image'
 
 export default function PostThumbnailImage(props){
-    const {tim, tn_w, tn_h, filename} = props.post
+    const {tim, tn_w, tn_h, filename, ext} = props.post
     const {board, className} = props
 
     if(typeof tim === 'undefined') return null
 
     return (
         <div className={className}>
-            <Image
-                src={`https://i.4cdn.org/${board}/${tim}s.jpg`}
-                width={tn_w}
-                height={tn_h}
-                alt={filename}
-            />
+            <a href={`https://i.4cdn.org/${board}/${tim}${ext}`} target="_blank" rel="noreferrer" className="pointer-events-auto">
+                <Image
+                    src={`https://i.4cdn.org/${board}/${tim}s.jpg`}
+                    width={tn_w}
+                    height={tn_h}
+                    alt={filename}
+                />
+            </a>
+
         </div>
         )
 
