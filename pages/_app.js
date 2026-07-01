77df0b99a0ba062147b2fe7d3b04b05495a2cddf
Saad A <saadahmad96@hotmail.com>
2026-07-01 21:14:22 +0500

    feat: analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KD3CG888BB" strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            gtag('config', 'G-KD3CG888BB');
+          `}
+        </Script>
+        <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
